8a3d32f4486304df2b14c100ba10d7ee0b9f3024
kei <kei@github.com>
2024-09-18 22:38:45 +0900

    add apt transfer https

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@ lb config \
 		--apt apt \
 		--architecture amd64 \
 		--distribution stable \
-		--archive-areas "main contrib non-free non-free-firmware" \
+		--parent-archive-areas "main contrib non-free non-free-firmware" \
 		--mirror-bootstrap "http://deb.debian.org/debian" \
 		--mirror-chroot "http://deb.debian.org/debian" \
-		--bootappend-live "boot=live components locales=ja_JP.UTF-8 timezone=Asia/Tokyo debug=1" \
+		--bootappend-live "boot=live components locales=ja_JP.UTF-8 timezone=Asia/Tokyo keyboard-layouts=jp debug=1" \
 		--binary-image iso \
